68eec3e56f06fd8967a2f80321d71c1da097a81b
John Todd <johnbriantodd@netscape.net>
2014-12-17 19:44:08 -0800

    First draft of Programming Assignment 2
    * Verified that cachematrix stores a matrix and its inverse
    * Verified that cacheSolve will store a matrix's inverse
    * Added a .gitignore file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions uses R's scoping rules to store a matrix
+## and its inverse, saving processing time in case the inverse is
+## needed multiple times.
 
-## Write a short comment describing this function
+## makeCacheMatrix provides storage, setters, and getters
+## for a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		inverse <<- NULL
+	}
+	get <- function() x
+	setSolve <- function(inv) inverse <<- inv
+	getSolve <- function() inverse
+	list(set = set,
+         get = get,
+         setSolve = setSolve,
+         getSolve = getSolve
+	)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve stores a matrix's inverse in makeCacheMatrix, if it
+## doesn't already exist
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+	inv <- x$getSolve()
+	if(!is.null(inv)) {
+		message("getting cached inverse")
+		return(inv)
+	}
+	data <- x$get()
+	inv <- solve(data, ...)
+	x$setSolve(inv)
+	inv
 }
